fc241de09d9b0326289511db517bb72a3410608e
Jorge Machuca <jjorgemachuca@gmail.com>
2025-05-10 20:59:29 +0200

    details animations

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -31,7 +31,7 @@ function Hero() {
         <Link
           href={`/contact`}
           role="button"
-          className="relative animate-delay-600 animate-jiggle hover:animate-scale ease-in-out inline-flex h-10 font-rubik mb-6 items-center justify-center px-4 shadow-2xl border border-[#621316] bg-[#621316] hover:bg-stone-200 hover:text-bastien text-stone-200 overflow-hidden rounded-md focus:outline-none focus:ring-2 focus:ring-stone-200 focus:ring-offset-2 focus:ring-offset-stone-900 mt-8"
+          className="relative animate-delay-500 hover:animate-jiggle animate-blurred-fade-in inline-flex h-10 font-rubik mb-6 items-center justify-center px-4 shadow-2xl shadow-bastien border border-[#621316] bg-[#621316]   text-stone-200 overflow-hidden rounded-md focus:outline-none focus:ring-2 focus:ring-stone-200 focus:ring-offset-2 focus:ring-offset-stone-900 mt-8"
         >
           {t("BOOK_NOW")}
         </Link>
@@ -39,12 +39,12 @@ function Hero() {
 
       {/* Imagen */}
       <div className="flex justify-center items-center md:mt-0 relative z-10 w-full px-4">
-        <div className="relative w-full max-w-xs sm:max-w-md h-[350px] sm:h-[400px] md:h-[600px] xl:h-[700px] overflow-hidden rounded-xl">
+        <div className="relative w-full hover:scale-110 ease-in-out transition duration-300 max-w-xs sm:max-w-md h-[350px] sm:h-[400px] md:h-[600px] xl:h-[700px] overflow-hidden rounded-xl">
           <Image
             src={hero1}
             alt={t("NAME")}
             fill
-            className="rounded-xl object-cover transition-all duration-1000 ease-in-out hover:scale-110"
+            className="rounded-xl shadow-2xl shadow-bastien mask-r-from-97% mask-b-from-95% mask-from-0% mask-to-0% object-cover transition-all duration-1000 ease-in-out hover:scale-125"
             priority
           />
         </div>
