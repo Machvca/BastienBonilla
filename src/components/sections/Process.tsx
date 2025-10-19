36e9cb97648e2e1ed8833d7b9e86c68dce61c9db
Jorge Machuca <jjorgemachuca@gmail.com>
2025-10-19 23:22:17 +0200

    images in the process part

--- src/components/sections/Process.tsx
+++ src/components/sections/Process.tsx
@@ -74,7 +74,7 @@ export default function TimelineDemo() {
         </div>
       </BackgroundLines>
 
-      <div className="w-full h-96 grid grid-col-2 items-center justify-center bg-[url(../../public/assets/images/hero3.jpg)] bg-cover bg-center bg-no-repeat rounded-2xl shadow-[0_0_24px_rgba(34,42,53,0.06),0_1px_1px_rgba(0,0,0,0.05),0_0_0_1px_rgba(34,42,53,0.04),0_0_4px_rgba(34,42,53,0.08),0_16px_68px_rgba(47,48,55,0.05),0_1px_0_rgba(255,255,255,0.1)_inset]">
+      <div className="w-full h-96 grid grid-col-2 items-center justify-center bg-[url(../../public/assets/images/hero3.webp)] bg-cover bg-center bg-no-repeat rounded-2xl shadow-[0_0_24px_rgba(34,42,53,0.06),0_1px_1px_rgba(0,0,0,0.05),0_0_0_1px_rgba(34,42,53,0.04),0_0_4px_rgba(34,42,53,0.08),0_16px_68px_rgba(47,48,55,0.05),0_1px_0_rgba(255,255,255,0.1)_inset]">
         <h1 className="text-stone-200 text-4xl font-rubik -mb-60">{t("BOOK_ME")}</h1>
         <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-stone-900-400 focus:ring-offset-2 focus:ring-offset-stone-900-50">
           <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#621316_0%,stone-500_50%,#621316_10%)]" />
